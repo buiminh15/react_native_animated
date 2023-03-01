d3eb70a2c8a12493b43e208d71e647ef037bcf6e
bui.minh <buiminh15@gmail.com>
2023-03-01 11:20:49 +0700

    feat: add reactotron

--- App.tsx
+++ App.tsx
@@ -1,18 +1,14 @@
 /* eslint-disable react-native/no-inline-styles */
 import * as React from 'react';
 import {
-  StatusBar,
-  FlatList,
-  Image,
   Animated,
-  Text,
-  View,
   Dimensions,
+  Image,
+  StatusBar,
   StyleSheet,
-  TouchableOpacity,
-  TextBase,
+  View,
 } from 'react-native';
-const {width, height} = Dimensions.get('screen');
+const {width} = Dimensions.get('screen');
 
 const data = [
   'https://cdn.dribbble.com/users/3281732/screenshots/11192830/media/7690704fa8f0566d572a085637dd1eee.jpg?compress=1&resize=1200x1200',
